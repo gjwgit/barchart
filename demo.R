2a9ff68defe44070a8c21984a2629406b389466b
Graham <Graham.Williams@togaware.com>
2019-04-30 08:15:06 +0800

    Migrate to using mlhub R package.

--- demo.R
+++ demo.R
@@ -3,21 +3,20 @@
 #
 # Copyright 2018 Graham.Williams@togaware.com
 
-cat("=====================================
-Introducing Bar Charts with R ggplot2
-=====================================
+library(mlhub)
 
-The Australian weather dataset from the Rattle package (from R)
-is used to illustrate bar charts.
+mlcat("Introducing Bar Charts with R ggplot2",
+"The Australian weather dataset from the Rattle package (from R) is used
+to illustrate bar charts. The ggplot2 package is installed and loaded into
+R with:
+
+  install.packages(c('rattle', 'ggplot2'))
+  library(rattle)
+  library(ggplot2)
 
 These examples come from the book, Essentials of Data Science,
 by Graham Williams. Used with permission.
-Visit https://essentials.togaware.com for more details.
-
-Press the <Enter> key after each message to display the referenced plot.
-Close the graphic windows using Ctrl-W
-
-")
+Visit https://essentials.togaware.com for more details.")
 
 # Load required packages.
 
@@ -40,9 +39,6 @@ cb <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79
 #-----------------------------------------------------------------------
 # Prepare the Weather dataset.
 #-----------------------------------------------------------------------
-
-cat("Plot a bar chart to show the frequency of wind direction in the dataset: ")
-invisible(readChar("stdin", 1))
 
 dsname <- "weatherAUS"
 ds     <- get(dsname)
@@ -74,6 +70,22 @@ fill <- "rain_tomorrow"
 # A simple bar chart.
 #-----------------------------------------------------------------------
 
+mlask()
+
+mlcat("Simple Bar Chart",
+      "A simple bar chart is easy to generate using gpglot2 and can be used to
+understand the frequency of observations. Below we explore the frequency of
+different wind directions in the weather dataset.
+
+The pipeline passes the dataset on to ggplot() where the aesethetics are
+identified, simply associating the wind direction at 3pm with the x-axis.
+A bar chart geometry is added to the plot. The heights of the bars will be
+automatically determined from the dataset.
+
+  ds %>%
+    ggplot(aes(x=wind_dir_3pm)) +
+    geom_bar()")
+
 fname <- "weather_bar_basic.pdf"
 pdf(file=fname, width=8)
 ds %>%
@@ -81,14 +93,22 @@ ds %>%
   scale_y_continuous(labels=comma) +
   geom_bar()
 invisible(dev.off())
-system(sprintf("atril --preview %s", fname), ignore.stderr=TRUE, wait=FALSE)
+mlask()
+mlpreview(fname, begin="")
+mlask()
 
 #-----------------------------------------------------------------------
 # Incorporate stacked bars.
 #-----------------------------------------------------------------------
 
-cat("\nPlot a stacked bar chart to include an extra dimension (variable): ")
-invisible(readChar("stdin", 1))
+mlcat("Stacked Bars",
+      "We can plot a stacked bar chart to include an extra dimension (variable) in the
+presentation. Here we have stacked the variable rain_tomorrow which records
+whether there was rain reported on the following day.
+
+  ds %>%
+    ggplot(aes(x=wind_dir_3pm, fill=rain_tomorrow)) +
+    geom_bar()")
 
 fname <- "weather_bar_stacked.pdf"
 pdf(file=fname, width=8)
@@ -98,14 +118,18 @@ ds %>%
   geom_bar() +
   scale_fill_brewer(palette="Paired")
 invisible(dev.off())
-system(sprintf("atril --preview %s", fname), ignore.stderr=TRUE, wait=FALSE)
+mlask()
+mlpreview(fname, begin="")
+mlask()
 
 #-----------------------------------------------------------------------
 # Replace stacked bars with dodged bars.
 #-----------------------------------------------------------------------
 
-cat("\nPlotting dodged bars in the chart may be more informative: ")
-invisible(readChar("stdin", 1))
+mlcat("Dodged Bars",
+      "Often it is more effective to compare bars from a common baseline. The above
+bars can be plotted side-by-side by introducing the use of the position=
+argument when adding the bars to the plot. ")
 
 fname <- "weather_bar_dodged.pdf"
 pdf(file=fname, width=8)
@@ -115,14 +139,18 @@ ds %>%
   geom_bar(position="dodge") +
   scale_fill_brewer(palette="Paired")
 invisible(dev.off())
-system(sprintf("atril --preview %s", fname), ignore.stderr=TRUE, wait=FALSE)
+mlask()
+mlpreview(fname, begin="")
+mlask()
 
 #-----------------------------------------------------------------------
 # A carefully crafted bar chart.
 #-----------------------------------------------------------------------
 
-cat("\nPlot a more carefully crafted bar chart: ")
-invisible(readChar("stdin", 1))
+mlcat("Crafting the Bar Chart",
+      "We demonstrate here a more fully and carefully crafted bar chart as we would
+for our final presnetation of the data. Titles are added and the legend is placed
+inside the plot iself.")
 
 blues2 <- brewer.pal(4, "Paired")[1:2]
 
@@ -151,7 +179,5 @@ ds %>%
        y        = "Number of Days",
        fill     = "Tomorrow")
 invisible(dev.off())
-system(sprintf("atril --preview %s", fname), ignore.stderr=TRUE, wait=FALSE)
-
-cat("\nPress Enter to finish the demonstration: ")
-invisible(readChar("stdin", 1))
+mlask()
+mlpreview(fname, begin="")
